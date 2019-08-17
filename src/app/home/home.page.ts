81496dc0327afb21fac65c32f7fa1d7fcb366343
penalozajohn25 <johnmanuelpenaloza@gmail.com>
2019-08-17 22:53:39 +0000

    brightness

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Brightness } from '@ionic-native/brightness/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  brightnessVal = 0.40;
+  valueGetBrightness:any;
+  constructor(private brightness: Brightness) {
+    this.brightness.setBrightness(this.brightnessVal);
+  }
+  adjustBrightness(){
+    this.brightness.setBrightness(this.brightnessVal);
+  }
 }
